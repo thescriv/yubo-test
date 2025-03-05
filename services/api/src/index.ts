8385baa07abefa8cebc35f3852b58fb41c387ef4
thescriv <escriva.theoparis@gmail.com>
2025-03-05 14:38:27 +0100

    feat: init api and update dockerfile

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -1,5 +1,25 @@
-function mainApi() {
-    console.log("hello world api")
-}
+import express from 'express';
+import dotenv from 'dotenv'
 
-mainApi()
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+
+const PORT = process.env.PORT || 3000
+
+app.get('/swipes/stats', (req, res) => {
+    res.status(200).json({ dislikes: 0, likes: 0 })
+});
+
+app.post('/swipes', (req, res) => {
+    res.status(204)
+});
+
+app.post('/gdpr/erase-all', (req, res) => {
+    res.status(204)
+});
+
+app.listen(PORT, () => {
+    console.log(`API running on port ${PORT}`)
+});
